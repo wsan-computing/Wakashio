492b7fd6db4a4768396007650fdf1cecf5a1d8c4
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2023-09-13 17:56:34 +0900

    environment.development.tsにダミーのデータを記述。.gitignoreを修正。

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "apiKey_is--here",
+    authDomain: "wakashio-XXXX.firebaseapp.com",
+    projectId: "wakashio-XXXX",
+    storageBucket: "wakashio-XXXX.appspot.com",
+    messagingSenderId: "XXXXXXXXXXXX",
+    appId: "appId:is:here"
+  }
+};
